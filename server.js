5a93e33fe04410ffd296d4438f2ca7dd7429698b
chenyueb883 <chenyueb292@gmail.com>
2018-08-01 21:20:31 +0800

    测试“ git commit -m 测试“

---
+++ server.js
@@ -0,0 +1,3 @@
+const path = require('path');
+console.log(path.join(__dirname,'server.js'))
+console.log(__filename)
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const http = require('http');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+var website = {
+    publicPath: 'http://127.0.0.1:8000/'
+}
+module.exports = {
+    entry: {
+        cyb: path.join(__dirname,'index.js'),
+    },
+    devtool: 'inline-source-map',
+    output: {
+        filename: 'js/[name].bundle.js',
+        path: path.join(__dirname,'dist'),
+        publicPath: website.publicPath
+    },
+    plugins:[
+      new  HtmlWebpackPlugin({
+          title: "cyb react",
+          filename: 'cyb.html',
+          inject: true,
+          favicon: path.join(__dirname,'src/ico','favicon-20180801070618479.ico')
+      }),
+      new ExtractTextPlugin('style.css'),
+      // new CleanWebpackPlugin(['dist'])
+    ],
+    module:{
+        rules:[
+            {
+                test:/\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ["css-loader","sass-loader"]
+                })
+            },
+            {
+                test:/\.(jpg|jpeg|png|svg|gif)$/,
+                use:[{
+                    loader: 'url-loader',
+                    options:{
+                        limit: 50000
+                    }
+                }],
+            }
+        ]
+    },
+    devServer:{
+        index: 'cyb.html',
+        contentBase: path.join(__dirname,'dist'),
+        host: '127.0.0.1',
+        port: 8000,
+    }
+}
